957b390dc7e426aca7825d99fcf7f0bfefa91a4c
kimhan0421 <kimhan0421@naver.com>
2020-08-18 23:10:59 +0900

    update styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -14,7 +14,6 @@ const globalStyles = createGlobalStyle`
         font-size: 25px;
         background-color: #fffffc;
         color: #404A41;
-        padding-top:50px;
     }
 `;
 export default globalStyles;
