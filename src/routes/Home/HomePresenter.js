86539ab25a9c23a81d43f378d21de5b1dca43b38
kimhan0421 <kimhan0421@naver.com>
2020-08-20 22:48:42 +0900

    Home img

--- src/routes/Home/HomePresenter.js
+++ src/routes/Home/HomePresenter.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
-import './main.jpg';
+import css from "assets/css/img.css";
+import image1 from "assets/img/Home/bg1.jpg";
 
 
+const Container = styled.h2`
+    padding-top: 100px;
+    text-align: center;
+    font-size: 50px;
+    color: #C9AA79;
+`;
 
 const Explanation = styled.div`
-
     padding: 0px 250px;
     text-align: center;
     color: #404A41;
     
     
 `;
+
 const ButtonList = styled.ul`
     display: flex;  
     justify-content: center;
@@ -31,9 +38,9 @@ const ButtonLink = styled(Link)`
 
 const ButFi = styled.button`
     color: white;
-    background: teal;
+    background: #C9AA79;
     padding: 0.375rem 0.75rem;
-    border: 1px solid teal;
+    border: 3px solid #C9AA79;
     border-radius: 0.25rem;
     font-size: 1rem;
     line-height: 1.5;
@@ -41,24 +48,17 @@ const ButFi = styled.button`
 `;
 
 const ButSe = styled.button`
-    color: white;
-    background: teal;
+    color: #404A41;
+    background: white;
     padding: 0.375rem 0.75rem;
-    border: 1px solid teal;
+    border: 1px solid #404A41;
     border-radius: 0.25rem;
     font-size: 1rem;
     line-height: 1.5;
     margin:55px;
 `;
 
-const Container = styled.h2`
-    padding: 100px 0px;
-    text-align: center;
-    font-size: 50px;
-    color: #C9AA79;
-    
-    
-`;
+
 
 const A = styled.div`
 
@@ -94,20 +94,6 @@ const A1 = styled.div`
 
     color: #404A41;
     position: relative;
-
-`;
-
-const B1 = styled.button`
-
-    position: relative;
-    color: white;
-    background: teal;
-    padding: 0.375rem 0.75rem;
-    border: 1px solid teal;
-    border-radius: 0.25rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    margin:55px;
 
 `;
 
@@ -176,8 +162,11 @@ const HomePresenter = () => (
 
         <Container>
             <h2>WEPLER</h2>
-        </Container>
+            <img src={image1} alt="First slide" className="slick-image" /> 
+        </Container>        
+            
         <Explanation>
+        
             <div>
                 <FaQuoteLeft size="24" color="#404A41" />
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -200,7 +189,7 @@ const HomePresenter = () => (
 
                     <ButtonList>
                         <ButtonLink to="/hire">
-                            <B>참여하기</B>
+                        <ButFi>참여하기</ButFi>
                         </ButtonLink>
                     </ButtonList>
                 </Y>
@@ -214,7 +203,7 @@ const HomePresenter = () => (
 
                     <ButtonList>
                         <ButtonLink to="/hire">
-                                <B1>더보기</B1>
+                                <ButSe>더보기</ButSe>
                         </ButtonLink>
                     </ButtonList>    
                 </Y1>
