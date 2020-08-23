d0190f570346a94bc51402cdd663cd63ebd1492e
KimSH39 <63898043+KimSH39@users.noreply.github.com>
2020-08-24 02:54:03 +0900

    Update WeplerPresenter.js

--- src/routes/Weplers/WeplerPresenter.js
+++ src/routes/Weplers/WeplerPresenter.js
@@ -36,25 +36,19 @@ const WeplerPresenter = () => (
         <Container>
             <p>Story 1</p>
             <WeHead>
-                <h2>Welcome to WEPLER</h2>
+                <h2>위플러에 오신 것을 환영합니다!</h2>
                 <br/>
                 
             </WeHead>
-            <p>"WEPLER로 세상을 바라보는 시각을 넓혀요"</p>
+            <p>"위플러로 세상을 바라보는 시각을 넓혀요"</p>
 
             <WeBody>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quiepakis nostrud exercitation
-                ullamco laboris nsi ut aliquip ex ea comepmodo consetquat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cfgillum dolore eutpe fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt inpeku culpa qui officia deserunt
-                mollit anim id est laborum.
-                </p>       
+                <p>위플러는 사회적 약자와 일손이 필요한 요양원을 지역사회 대학생과 이어주는 서비스입니다. 대학생인 플러스 회원은 자신의 정보를 담은 프로필을 작성하고, 사회적 약자와 요양원인 플리즈 회원은 그 프로필을 검색해 플러스 회원을 고용하게 됩니다. 이 서비스를 통해 플리즈 회원은 원할 때, 원하는 위치에서, 원하는 내용으로 취미 활동을 즐길 수 있게 되며 플러스 회원은 자신의 학과 전공을 살려 플리즈 회원을 도울 수 있습니다. 플러스 회원이 플리즈 회원과 함께 활동하기 이전에는 전문가의 교육을 1 회 이상 수강해야 하며, 자세한 날짜나 시간, 시급 등은 서로 협의 하에 결정하게 됩니다.</p>       
             </WeBody>
             <br></br><br></br><br></br><br></br>
             <p fontsize="10px">Story 2</p>
             <WeHead>
-                <h2>WEPLER's TEAM</h2>
+                <h2>위플러의 팀원을 소개합니다.</h2>
                 <br></br>           
                 <img src={member1} alt="First slide" className="member-image" />
                 <img src={member2} alt="First slide" className="member-image" />
