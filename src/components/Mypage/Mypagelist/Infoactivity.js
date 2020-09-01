297281b1c234e55ce592885a2b13d0eae757e5b0
KimSH39 <63898043+KimSH39@users.noreply.github.com>
2020-09-02 06:57:13 +0900

    Add files via upload

---
+++ src/components/Mypage/Mypagelist/Infoactivity.js
@@ -0,0 +1,151 @@
+import React from "react";
+import styled from "styled-components";
+import HashTag from "components/HashTag/HashTagChoose/HashTagChoose";
+import WhenDay from "components/Sign/Plus/WhenDay";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#C9AA79',
+    },
+    secondary: {
+      main: '#EFEFEF',
+    },
+  },
+});
+
+const TextTitle = styled.div`
+  padding-top:0px;
+  font-weight: bold;
+  font-size:25px;
+`;
+
+const ButtonField = styled.ul`
+  display: flex;  
+  justify-content: center;
+  text-align: center;
+  padding-top:30px; 
+`;
+
+const TextQuestion = styled.div`
+  padding-top: 50px;
+  font-size:20px;
+  font-weight: bold;
+`;
+
+const DeleteBt = styled.div`
+  padding-left:40px;
+`;
+
+const CompleteBt = styled.div`
+  padding-right:40px;
+`;
+
+const Field = styled.div`
+  display: flex;  
+`;
+
+const FirstQuestion = styled.div`
+  padding-top:0px;
+`;
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '160%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <TextTitle>
+            활동 분야 수정
+          </TextTitle>
+        </div>
+      </Container>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <div className={classes.paper}>
+            <form className={classes.form} noValidate>
+              <Grid>
+                <Grid item xs={12}>
+                  <FirstQuestion>
+                    원하는 재능 나눔 분야
+                  </FirstQuestion>
+                </Grid>
+                <Field>
+                  <HashTag />
+                </Field>
+
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="봉사자 교육을 수료했습니다."
+                />
+
+                <WhenDay />
+              </Grid>
+
+              <ButtonField>
+                      <CompleteBt>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        color="secondary"
+                        className={classes.submit}
+                        margin="normal"
+                      >
+                        완료하기
+                    </Button>
+                    </CompleteBt>
+
+                    <DeleteBt>
+                      <Button
+                        type="submit"
+                        variant="contained"
+                        className={classes.submit}
+                        padding="normal"
+                      >
+                        취소하기
+                    </Button>
+                    </DeleteBt>
+                    </ButtonField>
+
+            </form>
+          </div>
+          <Box mt={5} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+}
+
---
+++ src/components/Mypage/Mypagenav.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
+
+
+
+const List = styled.ul`
+    text-align: center;
+    justify-content: center;
+`;
+
+const Item = styled.li`
+  padding-right: 1070px;
+  padding-top: 10px; 
+  font-size: 20px;
+  text-align: center;
+`;
+
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Menuname = styled.li`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #F2D4CA;
+`;
+
+export default withRouter(({location: {pathname} }) => (
+  <>
+
+
+    <List>
+      <Item>
+        <Menuname>개인 정보 수정</Menuname>
+      </Item>
+      <Item current={pathname === "/Infobasic"}>
+        <SLink to="/Infobasic">기본 정보</SLink>
+      </Item>
+      <Item current={pathname === "/Infoactivity"}>
+        <SLink to="/Infoactivity">활동 분야</SLink >
+      </Item>
+      <Item>
+        <Menuname>교육 이수 내역</Menuname>
+      </Item>
+      <Item current={pathname === "/Activityapply"}>
+        <SLink to="/Activityapply">신청 활동</SLink >
+      </Item>
+      <Item current={pathname === "/Activityend"}>
+        <SLink to="/Activityend">진행중</SLink >
+      </Item>
+      <Item current={pathname === "/Activityprogress"}>
+        <SLink to="/Activityprogress">활동 완료</SLink >
+      </Item>
+    </List>
+  
+  </>
+));
