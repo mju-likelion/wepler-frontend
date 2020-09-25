2dbdb42647fc44de1ee271ffe2635c96d482b1a3
KimSH39 <63898043+KimSH39@users.noreply.github.com>
2020-09-25 18:32:36 +0900

    Add files via upload

---
+++ src/components/MyPages/PlusMypage/PlusMypagelist/Basic/BasicStyle.js
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+
+export const TextTitle = styled.div`
+  padding-top: 10px;
+  padding-bottom: 30px;
+  padding-left: 200px;
+  font-weight: bold;
+  font-size: 25px;
+  text-align: center;
+`;
+
+export const BigProfile = styled.div`
+  text-align: center;
+  justify-content: center;
+`;
+
+export const BigBox = styled.div`
+  text-align: center;
+  justify-content: center;
+`;
+
+export const Profile = styled.div`
+  padding-right: 60px;
+  padding-bottom: 20px;
+  justify-content: center;
+  text-align: center;
+`;
+
+export const Infor = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+export const InforContents = styled.div`
+  color: #22577a;
+`;
+
+export const Infortalent = styled.div`
+  padding-bottom: 50px;
+  padding-left: 10%;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  text-align: center;
+`;
+
+export const ButtonList = styled.div`
+  padding-left: 20%;
+`;
+
+export const ActivityDisplay = styled.div`
+  display: flex;
+  margin: 30px;
+  line-height: 1.5;
+  input {
+    width: 10%;
+  }
+`;
+
+export const ActivityList = styled.div`
+  display: flex;
+`;
+
+export const TextWrapterm = styled.div`
+  display: flex;
+  font-size: 23px;
+  margin-top: 25px;
+  margin-left: 20px;
+  textarea {
+    resize: none;
+    border: none;
+    width: 30%;
+    height: 30px;
+    border: none;
+  }
+  div {
+    padding-left: 5px;
+    font-size: 20px;
+  }
+`;
+
+export const ActivityBox = styled.div`
+  padding: 5px 5px;
+  border-radius: 5px;
+  border: 2px solid #c9aa79;
+  background-color: #ffffff;
+  color: #404a41;
+  margin-right: 20px;
+  display: flex;
+  &:hover {
+    background-color: #f2d4ca;
+  }
+`;
