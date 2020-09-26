c8c7dba1c93691273bb810f9fe4f8ee3dc918f80
KimSH39 <63898043+KimSH39@users.noreply.github.com>
2020-09-26 22:18:52 +0900

    Add files via upload

---
+++ src/components/MyPages/PlusMypage/PlusMypagelist/ActivityIng/PlusActivityIngContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import ActivityIngPresenter from "./PlusActivityIngPresenter";
+
+class ActivityContainer extends Component {
+  state = {
+    ItemList: [], // 프로필 개수
+  };
+
+  componentDidMount() {
+    const data = require("../../../../../BoardData.json");
+    this.setState({
+      ItemList: data.mypage,
+    });
+  }
+
+  render() {
+    const { ItemList } = this.state;
+    return <ActivityIngPresenter Itemcard={ItemList} />;
+  }
+}
+export default ActivityContainer;
---
+++ src/components/MyPages/PlusMypage/PlusMypagelist/ActivityIng/PlusActivityIngDetail.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import PlusMypageNav from "../../PlusMypagenav";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import styled from "styled-components";
+
+const Plus = styled.div`
+  padding-bottom: 10px;
+  padding-top: 30px;
+`;
+
+const Box = styled.div`
+  padding-bottom: 30px;
+  padding-top: 30px;
+  padding-left: 30px;
+`;
+
+const Detail = ({ match }) => {
+  console.log(match.params.profileId);
+  const [plz_id, setplz_id] = useState("");
+  const [plz_email, setplz_email] = useState("");
+  const [plz_oneself, setplz_oneself] = useState("");
+  const [plz_rating, setplz_rating] = useState(5); //평점은 기본 5
+  const [plz_fields, setplz_fields] = useState([]);
+  const [plz_start_day, setplz_start_day] = useState([]);
+  const [plz_start_time, setplz_start_time] = useState("");
+  const [plz_end_time, setplz_end_time] = useState("");
+  const [plz_address_big, setplz_address_big] = useState("");
+  const [plz_address_small, setplz_address_small] = useState("");
+
+  useEffect(() => {
+    async function getApply() {
+      //   try {
+      //     //프로필의 내용
+      //     const reads = await axios.get(`/apply_detail/${match.params.profileId}`, {
+      //       headers: {
+      //         Authorization: JSON.parse(localStorage.getItem("token")),
+      //       },
+      //     });
+      //   } catch {
+      //     console.log("profile error!");
+      //   }
+      setplz_id("아이디1");
+      setplz_email("1234@naver.com");
+      setplz_oneself("나는야 플러스회원");
+      setplz_rating(5);
+      setplz_fields(["council", "trip"]);
+      setplz_start_day("mon");
+      setplz_start_time("8:00");
+      setplz_end_time("17:00");
+      setplz_address_big("seoul");
+      setplz_address_small("용인시 처인구 명지대학교");
+    }
+    getApply();
+  }, []);
+
+  return (
+    <>
+      <PlusMypageNav />
+      <Container maxWidth="xs">
+      <Grid>
+         <Box>
+          <Plus>
+          플리즈 회원 아이디 : {plz_id}
+          </Plus>
+          <Plus>
+          플리즈 회원 한 줄 소개 : {plz_oneself}
+          </Plus>
+          <Plus>
+          활동 분야 : {plz_fields}
+          </Plus>
+          <Plus>
+          활동 시작 날짜 : {plz_start_day}
+          </Plus>
+          <Plus>
+          활동 시작 시간 : {plz_start_time}
+          </Plus>
+          <Plus>
+          활동 마감 날짜 : {plz_end_time}
+          </Plus>
+          <Plus>
+          플리즈 회원 이메일 주소 : {plz_email}
+          </Plus>
+          <Plus>
+          플리즈 회원 활동 장소  : {plz_address_big}
+          </Plus>
+          <Plus>
+          플리즈 회원 자세한 활동 장소 : {plz_address_small}
+          </Plus>
+          </Box>          
+        </Grid>
+      </Container>
+    </>
+  );
+};
+export default Detail;
