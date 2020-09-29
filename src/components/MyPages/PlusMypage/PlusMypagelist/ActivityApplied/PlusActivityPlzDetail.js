f46db3204005a21096b83479c676d0c45c1f6086
kimhan0421 <kimhan0421@naver.com>
2020-09-29 23:02:31 +0900

    Mypage warning fix

--- src/components/MyPages/PlusMypage/PlusMypagelist/ActivityApplied/PlusActivityPlzDetail.js
+++ src/components/MyPages/PlusMypage/PlusMypagelist/ActivityApplied/PlusActivityPlzDetail.js
@@ -6,28 +6,26 @@ import { Plus, Container } from "../Basic/BasicStyle";
 import styled from "styled-components";
 
 const PlusId = styled.div`
-    padding-right: 380px;
-    padding-bottom: 30px;
-    padding-top: 30px;
-    text-align: center;
+  padding-right: 380px;
+  padding-bottom: 30px;
+  padding-top: 30px;
+  text-align: center;
 `;
 
 const PlusMail = styled.div`
-    padding-left: 330px;
-    padding-bottom: 10px;
+  padding-left: 330px;
+  padding-bottom: 10px;
 `;
 
 const Detail = ({ match }) => {
   console.log(match.params.profileId);
-  const [plz_id, setplz_id] = useState("");
-  const [plz_email, setplz_email] = useState("");
-  const [plz_oneself, setplz_oneself] = useState("");
-  const [plz_fields, setplz_fields] = useState([]);
-  const [plz_start_day, setplz_start_day] = useState([]);
-  const [plz_start_time, setplz_start_time] = useState("");
-  const [plz_end_time, setplz_end_time] = useState("");
-  const [plz_address_big, setplz_address_big] = useState("");
-  const [plz_address_small, setplz_address_small] = useState("");
+  const [plz_id, setPlz_id] = useState("");
+  const [plz_email, setPlz_email] = useState("");
+  const [plz_fields, setPlz_fields] = useState([]);
+  const [plz_start_time, setPlz_start_time] = useState("");
+  const [plz_end_time, setPlz_end_time] = useState("");
+  const [plz_address_big, setPlz_address_big] = useState("");
+  const [plz_address_small, setPlz_address_small] = useState("");
 
   useEffect(() => {
     async function getApply() {
@@ -41,15 +39,13 @@ const Detail = ({ match }) => {
       //   } catch {
       //     console.log("profile error!");
       //   }
-      setplz_id("아이디1");
-      setplz_email("1234@naver.com");
-      setplz_oneself("나는야 플리즈회원");
-      setplz_fields(["council", "trip"]);
-      setplz_start_day("mon");
-      setplz_start_time("8:00");
-      setplz_end_time("17:00");
-      setplz_address_big("seoul");
-      setplz_address_small("용인시 처인구 명지대학교");
+      setPlz_id("플리즈1");
+      setPlz_email("1234@naver.com");
+      setPlz_fields(["council", "trip"]);
+      setPlz_start_time("8:00");
+      setPlz_end_time("17:00");
+      setPlz_address_big("seoul");
+      setPlz_address_small("용인시 처인구 명지대학교");
     }
     getApply();
   }, []);
@@ -59,12 +55,14 @@ const Detail = ({ match }) => {
       <PlusMypageNav />
       <Container maxWidth="xs">
         <Grid>
-            <PlusId>{plz_id}</PlusId>
-            <PlusMail>이메일 : {plz_email}</PlusMail>
-            <Plus>분야 : {plz_fields}</Plus>
-            <Plus>시간 : {plz_start_time} ~ {plz_end_time}</Plus>
-            <Plus>지역 : {plz_address_big}</Plus>
-            <Plus>장소 : {plz_address_small}</Plus>
+          <PlusId>{plz_id}</PlusId>
+          <PlusMail>이메일 : {plz_email}</PlusMail>
+          <Plus>분야 : {plz_fields}</Plus>
+          <Plus>
+            시간 : {plz_start_time} ~ {plz_end_time}
+          </Plus>
+          <Plus>지역 : {plz_address_big}</Plus>
+          <Plus>장소 : {plz_address_small}</Plus>
         </Grid>
       </Container>
     </>
--- src/components/MyPages/PlzMypage/PlzMypagelist/ActivityIng/PlzActivityIngDetail.js
+++ src/components/MyPages/PlzMypage/PlzMypagelist/ActivityIng/PlzActivityIngDetail.js
@@ -6,25 +6,22 @@ import styled from "styled-components";
 import { Plus, Container } from "../Basic/PlzBasicStyle";
 
 const PlusId = styled.div`
-    padding-right: 380px;
-    padding-bottom: 30px;
-    padding-top: 30px;
-    text-align: center;
+  padding-right: 380px;
+  padding-bottom: 30px;
+  padding-top: 30px;
+  text-align: center;
 `;
 
 const PlusMail = styled.div`
-    padding-left: 330px;
-    padding-bottom: 10px;
+  padding-left: 330px;
+  padding-bottom: 10px;
 `;
 
 const Detail = ({ match }) => {
   console.log(match.params.profileId);
   const [plz_id, setplz_id] = useState("");
   const [plz_email, setplz_email] = useState("");
-  const [plz_oneself, setplz_oneself] = useState("");
-  const [plz_rating, setplz_rating] = useState(5); //평점은 기본 5
   const [plz_fields, setplz_fields] = useState([]);
-  const [plz_start_day, setplz_start_day] = useState([]);
   const [plz_start_time, setplz_start_time] = useState("");
   const [plz_end_time, setplz_end_time] = useState("");
   const [plz_address_big, setplz_address_big] = useState("");
@@ -44,10 +41,7 @@ const Detail = ({ match }) => {
       //   }
       setplz_id("아이디1");
       setplz_email("1234@naver.com");
-      setplz_oneself("나는야 플러스회원");
-      setplz_rating(5);
       setplz_fields(["council", "trip"]);
-      setplz_start_day("mon");
       setplz_start_time("8:00");
       setplz_end_time("17:00");
       setplz_address_big("seoul");
@@ -61,13 +55,14 @@ const Detail = ({ match }) => {
       <PlzMypageNav />
       <Container maxWidth="xs">
         <Grid>
-            <PlusId>{plz_id}</PlusId>
-            <PlusMail>이메일 : {plz_email}</PlusMail>
-            <Plus>평균 평점 : {plz_rating}</Plus>
-            <Plus>분야 : {plz_fields}</Plus>
-            <Plus>시간 : {plz_start_time} ~ {plz_end_time}</Plus>
-            <Plus>지역 : {plz_address_big}</Plus>
-            <Plus>장소 : {plz_address_small}</Plus>
+          <PlusId>{plz_id}</PlusId>
+          <PlusMail>이메일 : {plz_email}</PlusMail>
+          <Plus>분야 : {plz_fields}</Plus>
+          <Plus>
+            시간 : {plz_start_time} ~ {plz_end_time}
+          </Plus>
+          <Plus>지역 : {plz_address_big}</Plus>
+          <Plus>장소 : {plz_address_small}</Plus>
         </Grid>
       </Container>
     </>
