d5719cb4d4e28062f944f7140f3c1cf49e321ba5
kimhan0421 <kimhan0421@naver.com>
2020-10-04 03:33:44 +0900

    url update

--- src/components/Reviews/Plz/List/ListBoard.js
+++ src/components/Reviews/Plz/List/ListBoard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import axios from "axios";
+import axios from "axios";
 import Listpage from "./Listpage";
 import Footer from "../../../Footer/Footer";
 
